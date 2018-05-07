ec125e373e4469cc1088db134a8b821ad7abb3f8
yulilong <yulilong222q@outlook.com>
2018-05-07 12:54:39 +0800

    修改开发环境地址的端口号。

--- config/webpack.config.dev.js
+++ config/webpack.config.dev.js
@@ -13,7 +13,7 @@ module.exports = merge(baseWebpackConfig,{
      */
     devtool: 'cheap-module-source-map',
     devServer:{//通过来自「webpack-dev-server」的这些选项，能够通过多种方式改变其行为。
-        port:9000,//指定监听的端口号
+        port:8080,//指定监听的端口号
         contentBase:path.join(__dirname,'dist'),// 告诉服务器从哪来提供内容。只有在你想要提供静态文件时才需要。
         publicPath:'/',//用于确定从哪里提供bundle，并且此选项优先
         compress:true,//一切服务都启用「gzip」压缩
@@ -21,13 +21,5 @@ module.exports = merge(baseWebpackConfig,{
         //host:"172.16.1.91",//默认是一个localhost.如果希望外部服务器可以访问，指点成自己的电脑的ip地址，https://doc.webpack-china.org/configuration/dev-server/#devserver-host-cli-
         //quiet:true,//除了初始化启动信息之外的任何内容都不会被打印到控制台。这也就意味着来自webpack的错误或警告在控制台不可见
     },
-    plugins:[
-        new webpack.ProvidePlugin({
-            $: "jquery",//jquery
-            jQuery: "jquery",
-            "window.jQuery": "jquery",
-            _:"lodash"//lodash
-        })
-    ]
 })
 
