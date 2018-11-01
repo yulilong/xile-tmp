7c0182f7788e18bdd767491b32dedc582e7ad1b8
yulilong <yulilong222q@outlook.com>
2018-11-02 00:33:22 +0800

    合作伙伴页面设置脚部条在页面底部。

--- src/js/partner.js
+++ src/js/partner.js
@@ -1,3 +1,37 @@
 // 引入相关的样式
 require("../less/partner.less");
 require("./cooperative.js")
+
+
+function setFooterBottom(content, footer) {
+  function getWindowHeight() {
+    var windowHeight = 0;
+    if (document.compatMode == "CSS1Compat") {
+      windowHeight = document.documentElement.clientHeight;
+    } else {
+      windowHeight = document.body.clientHeight;
+    }
+    return windowHeight;
+  }
+
+  var browserHeight = Number(getWindowHeight());      // 浏览器宽度
+  var footHeight = Number(footer.offsetHeight);       // 脚部高度
+  var contentHeight = Number(content.offsetHeight);   // 内容高度
+  // 设置内容高度
+  function setHeight() {
+    browserHeight = Number(getWindowHeight());
+    if (browserHeight > contentHeight) {
+      content.style.minHeight = browserHeight - footHeight + 'px';
+    } else {
+      content.style.minHeight = '';
+    }
+  }
+  setHeight();
+  window.addEventListener('resize', setHeight);
+}
+
+
+// 执行代码
+var content = document.querySelector('.content');
+var footer = document.querySelector('.footer');
+setFooterBottom(content, footer);
